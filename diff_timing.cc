9c731e69c4f3315d6831428fd5761de1f1cb3e34
clint-richardson <clint.a.richardson@gmail.com>
2015-05-23 21:16:16 +0200

    updated to print hists for separate objects, but discrepancy betwen 730 and 740 path names is making comparison difficult

--- diff_timing.cc
+++ diff_timing.cc
@@ -72,8 +72,10 @@ void diff_timing(){
   for(int i=0; i<nmax; i++){
 
     string histname730 = "DQMData/Run 1/HLT/Run summary/TimerService/Running 1 processes/process HLTX/Paths/"+names730[i];
+    if(i>80) cout<<names730[i]<<endl;
     //cout<<histname730<<endl;
     for(int j =0; j < 100; j++){
+
       //cout<<"730: "<<names730[i]<<" 740: "<<names740[j]<<endl;
       if(!(names730[i]==names740[j]) )continue;
       string histname740 = "DQMData/Run 1/HLT/Run summary/TimerService/Running 1 processes/process HLTX/Paths/"+names740[i];
@@ -94,16 +96,43 @@ void diff_timing(){
       }
     }
     //reshist->SetBinName(i,names730[i].c_str());
-    cout<<"running hist: "<<i<<" out of "<<nmax<<endl;
+    if(i%10==0){
+      cout<<"running hist: "<<i<<" out of "<<nmax<<endl;
     //cout<<histname740<<endl;
+    }
   }
 
-  int jjet =0;
+  std::vector<std::string> vJetNames;
+  std::vector<float> vJetRes;
+  std::vector<std::string> vMuNames;
+  std::vector<float> vMuRes;
+  std::vector<std::string> vElNames;
+  std::vector<float> vElRes;
+  std::vector<std::string> vPhotNames;
+  std::vector<float> vPhotRes;
+  std::vector<std::string> vHTNames;
+  std::vector<float> vHTRes;
 
   for(int i=0;i<vres.size();i++){
     if(names730[viter[i]].find("Jet")!=string::npos){
-      jjet++;
+      vJetNames.push_back(names730[viter[i]]);
       vJetRes.push_back(vres[i]);
+    }
+    else if(names730[viter[i]].find("HT")!=string::npos){
+      vHTNames.push_back(names730[viter[i]]);
+      vHTRes.push_back(vres[i]);
+    }
+    else if(names730[viter[i]].find("El")!=string::npos){
+      vElNames.push_back(names730[viter[i]]);
+      vElRes.push_back(vres[i]);
+    }
+    else if(names730[viter[i]].find("Mu")!=string::npos){
+      vMuNames.push_back(names730[viter[i]]);
+      vMuRes.push_back(vres[i]);
+    }
+    else if(names730[viter[i]].find("Photon")!=string::npos){
+      vPhotNames.push_back(names730[viter[i]]);
+      vPhotRes.push_back(vres[i]);
     }
 
   }
@@ -111,16 +140,61 @@ void diff_timing(){
 
 
   //make a new histogram to plot results
-  TH1F* JetResHist = new TH1F("reshist","Percentage change from 73X to 74X for Jet Paths",vres.size(),0,vres.size());
-  TH1F* MuResHist = new TH1F("reshist","Percentage change from 73X to 74X for Muon Paths",vres.size(),0,vres.size());
-  TH1F* ElResHist = new TH1F("reshist","Percentage change from 73X to 74X for Electron Paths",vres.size(),0,vres.size());
-  TH1F* PhotonResHist = new TH1F("reshist","Percentage change from 73X to 74X for Photon Paths",vres.size(),0,vres.size());
+  TH1F* JetResHist = new TH1F("JetReshist","Percentage change from 73X to 74X for Jet Paths",vJetRes.size(),0,vJetRes.size());
+  TH1F* MuResHist = new TH1F("MuReshist","Percentage change from 73X to 74X for Muon Paths",vMuRes.size(),0,vMuRes.size());
+  TH1F* ElResHist = new TH1F("ElReshist","Percentage change from 73X to 74X for Electron Paths",vElRes.size(),0,vElRes.size());
+  TH1F* PhotResHist = new TH1F("PhotReshist","Percentage change from 73X to 74X for Photon Paths",vPhotRes.size(),0,vPhotRes.size());
+  TH1F* HTResHist = new TH1F("HTReshist","Percentage change from 73X to 74X for HT Paths",vHTRes.size(),0,vHTRes.size());
 
+  TCanvas c1;
+  for(int j=0; j< vJetRes.size(); j++){
+    JetReshist->Fill(j+1,vJetRes[j]);
+    JetReshist->GetXaxis()->SetBinLabel(j+1,vJetNames[j].c_str());
+    cout<<"bin number: "<<j<<" with label "<<vJetNames[j]<<" and difference: "<<vJetRes[j]<<endl;
+  }
 
-      reshist->Fill(j,vres[i]);
-      reshist->GetXaxis()->SetBinLabel(j,names730[viter[i]].c_str());
-      cout<<"bin number: "<<j<<" with label "<<names730[viter[i]]<<" and difference: "<<vres[i]<<endl;
+  JetReshist->Draw();
+  c1.Print("TimingDifferencesForJetPaths.pdf");
 
-  reshist->Draw();
+  TCanvas c2;
+  for(int j=0; j< vHTRes.size(); j++){
+    HTReshist->Fill(j+1,vHTRes[j]);
+    HTReshist->GetXaxis()->SetBinLabel(j+1,vHTNames[j].c_str());
+    cout<<"bin number: "<<j<<" with label "<<vHTNames[j]<<" and difference: "<<vHTRes[j]<<endl;
+  }
+
+  HTReshist->Draw();
+  c2.Print("TimingDifferencesForHTPaths.pdf");
+
+  TCanvas c3;
+  for(int j=0; j< vElRes.size(); j++){
+    ElReshist->Fill(j+1,vElRes[j]);
+    ElReshist->GetXaxis()->SetBinLabel(j+1,vElNames[j].c_str());
+    cout<<"bin number: "<<j<<" with label "<<vElNames[j]<<" and difference: "<<vElRes[j]<<endl;
+  }
+
+  ElReshist->Draw();
+  c3.Print("TimingDifferencesForElPaths.pdf");
+
+  TCanvas c4;
+  for(int j=0; j< vMuRes.size(); j++){
+    MuReshist->Fill(j+1,vMuRes[j]);
+    MuReshist->GetXaxis()->SetBinLabel(j+1,vMuNames[j].c_str());
+    cout<<"bin number: "<<j<<" with label "<<vMuNames[j]<<" and difference: "<<vMuRes[j]<<endl;
+  }
+
+  MuReshist->Draw();
+  c4.Print("TimingDifferencesForMuPaths.pdf");
+
+  TCanvas c5;
+  for(int j=0; j< vPhotRes.size(); j++){
+    PhotReshist->Fill(j+1,vPhotRes[j]);
+    PhotReshist->GetXaxis()->SetBinLabel(j+1,vPhotNames[j].c_str());
+    cout<<"bin number: "<<j<<" with label "<<vPhotNames[j]<<" and difference: "<<vPhotRes[j]<<endl;
+  }
+
+  PhotReshist->Draw();
+  c5.Print("TimingDifferencesForPhotonPaths.pdf");
+
 
 }
