6fd0af6993e35b9cc23cec75ac67575b17b8caae
clint-richardson <clint.a.richardson@gmail.com>
2015-06-11 22:40:40 +0200

    added parameters for process name and run number

--- TimingAndRates.py
+++ TimingAndRates.py
@@ -10,11 +10,13 @@ parser.add_argument("--mc", dest='mc',action='store_true', help='specify to run 
 parser.add_argument("--data",dest='data',action='store_true', help='specify to run on data, if you use this you must also specify lumi sections')
 parser.add_argument("--time",type=float, help='for MC: the amount of time in the detector the sample represents',nargs=1)
 parser.add_argument("--lumis",type=float, help='for Data: the number of lumi sections run', nargs=1)
+parser.add_argument("--run",type=int, help="for Data: specify the run number",nargs=1)
+parser.add_argument("--process",type=str, help="In case the name of the process used is not HLTX specify it with this",nargs=1)
 
 args = parser.parse_args()
 
 nSeconds=0.0
-
+run=1
 if args.mc:
     if not args.time:
         print "Error! Need to specify time when running with mc option"
@@ -30,9 +32,13 @@ if args.data:
     if not args.lumis:
         print "Error! Need to specify lumis when running on data"
         quit()
+    elif not args.run:
+        print "Error! Need to specify run number"
+        quit()
     else:
         nSeconds = args.lumis[0] * 23.3
-        print "running data with %f lumis corresponding to %f seconds" % (args.lumis[0],nSeconds)
+        run=args.run[0]
+        print "running data with %f lumis corresponding to %f seconds for run number %i" % (args.lumis[0],nSeconds,run)
 
 
 import os
@@ -41,7 +47,13 @@ cmdstring=''
 
 if args.outfile:
     print 'Customizing to make output file', args.outfile[0]
-    cmdstring='root -b -q -l \'TimingAndRates.cc(%f,\"%s\",\"%s\")\'' % (nSeconds,args.inputfile[0],args.outfile[0])
+    if args.data:
+        cmdstring='root -b -q -l \'TimingAndRates.cc(%f,\"%s\",%i,\"%s\",\"HLT\")\'' % (nSeconds,args.inputfile[0],run,args.outfile[0])
+    else:
+        if args.process:
+            cmdstring='root -b -q -l \'TimingAndRates.cc(%f,\"%s\",1,\"%s\",\"%s\")\'' % (nSeconds,args.inputfile[0],args.outfile[0],args.process[0])
+        else:
+                        cmdstring='root -b -q -l \'TimingAndRates.cc(%f,\"%s\",1,\"%s\")\'' % (nSeconds,args.inputfile[0],args.outfile[0])
 else:
     print 'Running with default output file name'
     cmdstring='root -b -q -l \'TimingAndRates.cc(%f,\"%s\")\'' % (nSeconds,args.inputfile[0])
