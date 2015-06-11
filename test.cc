b07dd14f956fdfe078c67ee02e1b2bb9b30bd1e1
clint-richardson <clint.a.richardson@gmail.com>
2015-06-11 18:29:37 +0200

    added parameters for filename and time, and made a second loop for timing

--- test.cc
+++ test.cc
@@ -4,11 +4,13 @@
 #include "TFile.h"
 #include "TKey.h"
 #include "TCollection.h"
-
+#include <iostream>
+#include <string>
+#include "TROOT.h"
 using namespace std;
 
-void test(){
-  gStyle->SetOptStat(kFALSE);
+void test(float time, std::string filename){
+
 
   vector<string> vNames;
   vector<float> vTimes;
@@ -17,19 +19,17 @@ void test(){
 
   ofstream outfile("HLT_Paths_Total_Time_CMSSW742_pu40bx25.csv");
 
-  TFile* file703 = new TFile("DQM_Timing_PU40bx25_CMSSW742_May2015FrozenMenu.root");
-  file703->cd("DQMData/Run 1/HLT/Run summary/TimerService/Running 1 processes/process HLTX/Paths");
+  TFile* file = new TFile(filename.c_str());
+  file->cd("DQMData/Run 1/HLT/Run summary/TimerService/Running 1 processes/process HLTX/Paths");
 
-  TIter next703(gDirectory->GetListOfKeys());
-  TKey *key703;
-
-
+  TIter next(gDirectory->GetListOfKeys());
+  TKey *key;
   
-  while ((key703 = (TKey*)next703())) {
-    TClass *cl = gROOT->GetClass(key703->GetClassName());
+  while ((key = (TKey*)next())) {
+    TClass *cl = gROOT->GetClass(key->GetClassName());
     if (!cl->InheritsFrom("TH1")) continue;
-    TH1 *h = (TH1*)key703->ReadObj();
-    //    names703.push_back(h->GetName());
+    TH1 *h = (TH1*)key->ReadObj();
+    //    names.push_back(h->GetName());
     string title = h->GetTitle();
     string name = h->GetName();
     size_t pos;
@@ -52,15 +52,35 @@ void test(){
 	string label = axis->GetBinLabel(i);
 	if(label.find("hltBoolEnd")!=string::npos){
        	  //push back rate to vector
-	  rate = h->GetBinContent(i)/(1.621);
+	  rate = h->GetBinContent(i)/(time);
 	  vRates.push_back(rate);
 	  vNames.push_back(path);
-	  std::cout<<"path in rates: "<<path;
 	}
       }
-
+      
     }
 
+  }
+
+  TIter next2(gDirectory->GetListOfKeys());
+  TKey *key2;
+
+  while ((key2 = (TKey*)next2())) {
+    TClass *cl = gROOT->GetClass(key2->GetClassName());
+    if (!cl->InheritsFrom("TH1")) continue;
+    TH1 *h = (TH1*)key2->ReadObj();
+    //    names.push_back(h->GetName());
+    string title = h->GetTitle();
+    string name = h->GetName();
+    size_t pos;
+
+    if(title.find("v2")!=string::npos){
+      pos = title.find("v2");
+    }
+    else if(title.find("v1")!=string::npos){
+      pos=title.find("v1");
+    }
+    string path(title,0,pos-1);
     if(name.find("total")!=string::npos && name.find("module_total")==string::npos){
       //handle special case with 'totalOR' in name
       if(name.find("totalOR")!=string::npos && !(name.find("total",name.find("total"+1))!=string::npos)) continue;
@@ -70,32 +90,15 @@ void test(){
       vTimes.push_back(mean);
       std::cout<<" path in timing: "<<path<<std::endl;
     }
-
   }
 
-  std::cout<<vNames.size()<<" "<<vTimes.size()<<std::endl;
+
 
   for(unsigned int j =0; j<vNames.size(); j++){
     outfile<<vNames.at(j)<<","<<vTimes.at(j)<<","<<vRates.at(j)<<std::endl;
   }
 
-  //  TH1F* hist = new TH1F("hist","Paths Total time",names703.size(),0,names703.size());
-  /*  for(int i=0;i<names703.size();i++){
-    hist->Fill(i,avgs[i]);
-    hist->GetXaxis()->SetBinLabel(i+1,names703[i].c_str());   
-    //cout<<"Hist: "<<names703[i]<<" Average: "<<avgs[i]<<endl;
-  }
-  TCanvas c1;
-  hist->GetYaxis()->SetTitle("processing time (ms)");
-  //hist->Draw();
-  //  c1.Print("Paths_Total_Times.pdf");
 
-  TCanvas c2;
-  TH1F* aphist = file703->Get("DQMData/Run 1/HLT/Run summary/TimerService/Running 1 processes/process TEST/all_paths");
-  aphist->SetTitle("all_paths Processing time for GRunV41");
-    //cout<<histname703<<endl;
-  //aphist->Draw();
-  //c2.Print("GrunV41_allpaths.pdf");*/
 }
 
 
