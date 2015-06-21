1fa0cea309d42e300e60c3dc4b66f1b3530ddee0
Clint <clint.a.richardson@gmail.com>
2015-06-21 11:17:39 +0200

    added arguments

--- validation_plot.py
+++ validation_plot.py
@@ -1,17 +1,24 @@
 #/usr/bin/python
 
+#setup argument parser
+import argparse
 
-from ROOT import gROOT, TCanvas, TF1
- 
+parser = argparse.ArgumentParser(description='A script to plot several timing histograms on top of one another. Pass it arguments for files, runs, and, if needed process names (assumes HLTX by default). For more than one file the arguments should be comma separated lists ordered respectively. Makes an output file called validation_plot.pdf Usage: python validation_plot.py --inputfiles INPUTFILES --runs RUNNUMBERS --processes PROCESSNAMES')
+
+
+parser.add_argument("--inputfiles", type=str, help='The list of input files, comma separated if more than one file',required=True,nargs=1)
+parser.add_argument("--runs",type=str,help='the corresponding run numbers, set to 1 by default',nargs=1)
+parser.add_argument("--processes",type=str,help='the corresponding process names, set to HLTX by default',nargs=1)
+
+args=parser.parse_args()
+
+from ROOT import gROOT, TCanvas, TH1, TFile 
+
+#clear memory 
 gROOT.Reset()
-c1 = TCanvas( 'c1', 'Example with Formula', 200, 10, 700, 500 )
- 
-#
-# Create a one dimensional function and draw it
-#
-fun1 = TF1( 'fun1', 'abs(sin(x)/x)', 0, 10 )
-c1.SetGridx()
-c1.SetGridy()
-fun1.Draw()
-c1.Update()
+#make canvas to save plots to
+c1 = TCanvas('c1')
 
+
+
+ 
