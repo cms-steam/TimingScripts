13c4d2c7ae819a63f92ff26dbfbef1a5fccd790a
Clint <clint.a.richardson@gmail.com>
2016-02-02 16:25:55 +0100

    organization is fun!

---
+++ MachineStudies/getskims.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+### pu 63 (run 198609, ls 50, 76k events) - no out-of-time pileup*
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0050_part1.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0050_part2.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0050_part3.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0050_part4.root ./
+
+
+### pu 57 (run 198609, ls 107..111, 53k events) **- no out-of-time pileup*
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0107_part1.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0107_part2.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0108_part1.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0108_part2.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0109_part1.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0109_part2.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0110_part1.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0110_part2.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0111_part1.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198609/run198609_lumi0111_part2.root ./
+
+# pu 44 (run 198603, ls 34..37, 53k events) - no out-of-time pileup*
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0034_part1.root ./
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0034_part2.root ./
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0035_part1.root ./
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0035_part2.root ./
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0036_part1.root ./
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0036_part2.root ./
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0037_part1.root ./
+xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run198603/run198603_lumi0037_part2.root ./
+
+### pu 30 (run 207515, ls 183..218, 50k events) - 50 ns bunch spacing*
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi183.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi184.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi185.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi186.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi187.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi188.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi189.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi190.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi191.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi192.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi193.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi194.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi195.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi196.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi197.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi198.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi199.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi200.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi201.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi202.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi203.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi204.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi205.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi206.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi207.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi208.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi211.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi212.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi213.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi214.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi215.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi216.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi217.root ./
+#xrdcp root://eoscms.cern.ch//eos/cms/store/group/comm_trigger/TriggerStudiesGroup/Timing/run207515/run207515_lumi218.root ./
