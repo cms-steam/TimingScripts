5d19dddec8439c71af77cf1addafa6a6e2e83299
Clint <clint.a.richardson@gmail.com>
2016-02-09 13:35:25 +0100

    added work to configure cpu scan

--- Classes/helperFunctions.py
+++ Classes/helperFunctions.py
@@ -20,3 +20,26 @@ def writeMultiTestFile(mt,f):
         writeTestFile(t,f)
         #write blank line to separate test info
         f.write("\n")
+
+def writeRunLine(core,filename,logname):
+    return "nohup taskset -c %i cmsRun %s >& %s" % (core,filename,logname)
+
+
+def configureCPUScan(arch,name):
+ 
+    cpus = {'1':'SandyBridge','2':'IvyBridge','3':'Haswell'}
+
+    maxjobs = {'1':32,'2':32,'3':48}
+
+    print 'Configuring CPU scan for %s machine, which has max number of jobs %i. Would you like to run this or up to a smaller number?' % (cpus[arch],maxjobs[arch])
+    nJobs = int(raw_input('Enter desired number of jobs'))
+
+
+
+def configureOccScan(arch,name):
+
+
+def configureThreadScan(arch,name):
+
+
+def configureFreeScan(arch,name):
