77c8625e4f9d81909f9ca18113a96996e7626f19
Clint <clint.a.richardson@gmail.com>
2016-02-10 14:32:13 +0100

    added functions to add timer,throughput, and dqm to menu

--- Classes/helperFunctions.py
+++ Classes/helperFunctions.py
@@ -24,6 +24,85 @@ def writeMultiTestFile(mt,f):
 def writeRunLine(core,filename,logname):
     return "nohup taskset -c %i cmsRun %s >& %s" % (core,filename,logname)
 
+def getFastTimerService():
+    return """
+
+process.FastTimerService = cms.Service( "FastTimerService",
+    dqmPath = cms.untracked.string( "HLT/TimerService" ),
+    dqmModuleTimeRange = cms.untracked.double( 400.0 ),
+    useRealTimeClock = cms.untracked.bool( True ),
+    enableTimingModules = cms.untracked.bool( True ),
+    enableDQM = cms.untracked.bool( True ),
+    enableDQMbyModule = cms.untracked.bool( False ),
+    enableTimingExclusive = cms.untracked.bool( True ),
+    skipFirstPath = cms.untracked.bool( True ),
+    enableDQMbyLumiSection = cms.untracked.bool( True ),
+    dqmPathTimeResolution = cms.untracked.double( 0.5 ),
+    dqmPathTimeRange = cms.untracked.double( 1000.0 ),
+    dqmTimeRange = cms.untracked.double( 10000.0 ),
+    dqmLumiSectionsRange = cms.untracked.uint32( 2500 ),
+    enableDQMbyProcesses = cms.untracked.bool( True ),
+    enableDQMSummary = cms.untracked.bool( True ),
+    enableTimingSummary = cms.untracked.bool( True ),
+    enableDQMbyPathTotal = cms.untracked.bool( True ),
+    enableTimingPaths = cms.untracked.bool( True ),
+    enableDQMbyPathExclusive = cms.untracked.bool( False ),
+    dqmTimeResolution = cms.untracked.double( 5.0 ),
+    dqmModuleTimeResolution = cms.untracked.double( 0.2 ),
+    enableDQMbyPathActive = cms.untracked.bool( False ),
+    enableDQMbyPathDetails = cms.untracked.bool( False ),
+    enableDQMbyPathOverhead = cms.untracked.bool( False ),
+    enableDQMbyPathCounters = cms.untracked.bool( True ),
+    enableDQMbyModuleType = cms.untracked.bool( False )
+)
+"""
+
+
+def getThroughputService():
+    return """
+
+process.ThroughputService = cms.Service('ThroughputService',
+  timeRange = cms.untracked.double(60000),
+  timeResolution = cms.untracked.double(10),
+  dqmPath = cms.untracked.string('HLT/Throughput')
+)"""
+
+
+def getDQMModule():
+    return """
+
+# FastTimerServiceClient                                                                                                                                                                                          
+process.fastTimerServiceClient = cms.EDAnalyzer( "FastTimerServiceClient",
+    dqmPath = cms.untracked.string( "HLT/TimerService" )
+)
+
+# DQM file saver                                                                                                                                                                                                  
+process.dqmFileSaver = cms.EDAnalyzer( "DQMFileSaver",
+    convention        = cms.untracked.string( "Offline" ),
+    workflow          = cms.untracked.string( "/HLT/FastTimerService/All" ),
+    dirName           = cms.untracked.string( "." ),
+    saveByRun         = cms.untracked.int32(1),
+    saveByLumiSection = cms.untracked.int32(-1),
+    saveByEvent       = cms.untracked.int32(-1),
+    saveByTime        = cms.untracked.int32(-1),
+    saveByMinute      = cms.untracked.int32(-1),
+    saveAtJobEnd      = cms.untracked.bool(False),
+    forceRunNumber    = cms.untracked.int32(-1),
+)
+
+process.TimingOutput = cms.EndPath( process.fastTimerServiceClient + process.dqmFileSaver )"""
+
+def addTimingAndThroughputToMenu(menu):
+
+    #open file to append it
+    hlt = open(menu,'a')
+    
+    #add the timer service:
+    hlt.write(getFastTimerService)
+    #add the throughput service:
+    hlt.write(getThroughputService)
+    ##add the DQM output module
+    hlt.write(getDQMModule)
 
 def copyHltMenu(t):
     ncores = t.ncores
