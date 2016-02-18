12ed4d9c53d0326e86534c7eee862c1f64dc3fd9
Clint <clint.a.richardson@gmail.com>
2016-02-18 10:06:54 +0100

    added read multitest and updated multitest addTest function

--- Classes/test.py
+++ Classes/test.py
@@ -20,5 +20,40 @@ class multiTest(object):
         self.name = name
         self.tests = []
 
-    def addTest(test):
-        tests.append(test)
+    def addTest(self,t):
+        self.tests.append(t)
+
+
+def readTest(tfile):
+    return
+
+def readMultiTest(tfile):
+
+    f = open(tfile,'r')
+
+    name = (tfile.split('.txt')[0]).split('cfg_')[1]
+
+    mt = multiTest(name)
+
+    for line in f:
+        #read in configuration from first text
+        if line.find('ncores')!=-1:
+            ncores = int(line.split(':')[1])
+        if line.find('njobs')!=-1:
+            njobs = int(line.split(':')[1])
+        if line.find('nthreads')!=-1:
+            nthreads = int(line.split(':')[1])
+        if line.find('menu')!=-1:
+            menu = line.split(':')[1]
+        if line.find('trials')!=-1:
+            ntrials = int(line.split(':')[1])
+
+        #if line is newline means we have reach end of test so add it to mtest
+        if line == '\n':
+            t = test(njobs,ncores,nthreads,name,menu,ntrials)
+            mt.addTest(t)
+            
+    f.close()
+    
+
+    return mt
