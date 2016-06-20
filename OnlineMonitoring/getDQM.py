d1b20af2bf9bedefa514e3b0fa21251bd3dba130
Clint <clint.a.richardson@gmail.com>
2016-06-20 16:39:21 +0200

    added throughput service

--- OnlineMonitoring/getDQM.py
+++ OnlineMonitoring/getDQM.py
@@ -3,8 +3,9 @@
 import os
 from ROOT import *
 
-runs = [274998,274956,274958,274959,274968,274969,274971,274443,274442,274441,274440,274422,274421,274420,274388,274387,274345,274344,274338,274335,274319,274316,274315,274314,274286,274284,274282]
-#runs = [274443,274442,274441,274440,274422,274421,274420,274388,274387,274345,274344,274338,274335,274319,274316,274315,274314,274286,274284,274282]
+runs = [275319,275326,275337,275338,275344,275345,275370,275371,275375,275376]
+#runs = [275125,275124,275074,275073,275068,275067,275066,275059,275001,275000,274999,274998,274956,274958,274959,274968,274969,274971,274443,274442,274441,274440,274422,274421,274420,274388,274387,274345,274344,274338,274335,274319,274316,274315,274314,274286,274284,274282,275282,275283,275284,275286,275290,275291,275292,275293,275309,275310,275311]
+
 
 #for run in runs:
 #    f = open("json_%i.txt" %run,"w")
@@ -17,8 +18,8 @@ if not os.path.isdir("./event_rootFiles"):
     os.system("mkdir event_rootFiles")
 if not os.path.isdir("./timeByLs_rootFiles"):
     os.system("mkdir timeByLs_rootFiles")
-if not os.path.isdir("./lumi_rootFiles"):
-    os.system("mkdir lumi_rootFiles")
+if not os.path.isdir("./throughput_rootFiles"):
+    os.system("mkdir throughput_rootFiles")
 
 
 for run in runs:
@@ -32,5 +33,9 @@ for run in runs:
     #cd to lumi directory
     os.chdir('../lumi_rootFiles')
     command ="python ../dqm-acces.py -s https://cmsweb.cern.ch/dqm/online/data/json -f \"/Scal/LumiScalers/Instant_Lumi\" -e \"run == %i and match(\'/Global/Online/ALL\', dataset)\" -r -w" % run
+    os.system(command)
+    #throughput
+    os.chdir('../throughput_rootFiles')
+    command ="python ../dqm-acces.py -s https://cmsweb.cern.ch/dqm/online/data/json -f \"/HLT/Throughput/throughput_retired\" -e \"run == %i and match(\'/Global/Online/ALL\', dataset)\" -r -w" % run
     os.system(command)
     os.chdir('..')
