7c78cb446d4e5a3997c923777f5e87cf12ba2954
Clint <clint.a.richardson@gmail.com>
2016-07-18 16:45:08 +0200

    added plot vs pileup

--- OnlineMonitoring/plotDQM.py
+++ OnlineMonitoring/plotDQM.py
@@ -3,9 +3,9 @@
 import os
 from ROOT import *
 #runs = [275125,275124,275074,275073,275068,275067,275066,275059,275001,275000,274999,274998,274971,274969,274968,274959,274958,274956,274443,274442,274441,274440,274422,274421,274420,274388,274387,274345,274344,274338,274335,274319,274316,274315,274314,274286,274284,274282,275282,275283,275284,275286,275290,275291,275292,275293]
-runs=[274282, 274284, 274286, 274314, 274315, 274316, 274319, 274335, 274338, 274344, 274345, 274387, 274388, 274420, 274421, 274422, 274440, 274441, 274442, 274443, 274956, 274958, 274959, 274968, 274969, 274971, 274998, 274999, 275000, 275001, 275059, 275066, 275067, 275068, 275073, 275074, 275124, 275125, 275282,275283,275284,275286,275290,275291,275292,275293,275309,275310,275311,275319,275326,275337,275338,275344,275345,275370,275371,275375]
-thruRuns=[275319,275326,275337,275338,275344,275345,275370,275371,275375]
-
+runs=[274282, 274284, 274286, 274314, 274315, 274316, 274319, 274335, 274338, 274344, 274345, 274387, 274388, 274420, 274421, 274422, 274440, 274441, 274442, 274443, 274956, 274958, 274959, 274968, 274969, 274971, 274998, 274999, 275000, 275001, 275059, 275066, 275067, 275068, 275073, 275074, 275124, 275125, 275282,275283,275284,275286,275290,275291,275292,275293,275309,275310,275311,275319,275326,275337,275338,275344,275345,275370,275371,275375,275376,275657,275658,275757,275768,275772,275774,275776,275777,275778,275782,275783,275828,275832,275833,275834,275836,275837,275847,275886,275887,275890,275911,275912,275913,275918,275920,275923,275931,275963,276092,276097,276242,276243,276244,276282,276283,276315,276317,276318,276327,276352,276355,276357,276361,276363,276384,276437,276453,276454,276455,276456,276458,276495,276501,276502,276525,276527,276528,276542,276544,276544,276545,276581,276582,276585,276586,276587,276653,276655,276659,276775,276776,276794,276807,276808,276810,276811,276831,276834,276870,276935]
+thruRuns=[275319,275326,275337,275338,275344,275345,275370,275371,275375,275376,275657,275658,275757,275768,275772,275774,275776,275777,275778,275782,275783,275828,275832,275833,275834,275836,275837,275847,275886,275887,275890,275911,275912,275913,275918,275920,275923,275931,275963,276092,276097,276242,276243,276244,276282,276283,276315,276317,276318,276327,276352,276355,276357,276361,276363,276384,276437,276453,276454,276455,276456,276458,276495,276501,276502,276525,276527,276528,276542,276544,276544,276545,276581,276582,276585,276586,276587,276653,276655,276659,276775,276776,276794,276807,276808,276810,276811,276831,276834,276870,276935]
+print len(runs), len(thruRuns)
 timingFiles = []
 timeByLsFiles = []
 lumiFiles = []
@@ -35,7 +35,7 @@ for f in timingFiles:
 timingByLsHists = []
 
 for f in timeByLsFiles:
-    hist = f.Get("/HLT/TimerService/Running 4 processes/event_byls")
+    hist = (f.Get("/HLT/TimerService/Running 4 processes/event_byls"))
     timingByLsHists.append(hist)
 
 lumiHists = []
@@ -50,12 +50,122 @@ for f in thruFiles:
     hist = f.Get("/HLT/Throughput/throughput_retired")
     thruHists.append(hist)
 
-print len(runs)
+
 #now define for fills
-runs5030= runs[57:58]
-tbls5030=timingByLsHists[57:58]
-lumi5030=lumiHists[57:58]
-thru5030=thruHists[8:9]
+runs5101= runs[139:140]
+tbls5101=timingByLsHists[139:140]
+lumi5101=lumiHists[139:140]
+thru5101=thruHists[90:91]
+
+runs5097= runs[138:139]
+tbls5097=timingByLsHists[138:139]
+lumi5097=lumiHists[138:139]
+thru5097=thruHists[89:90]
+
+runs5096= runs[136:138]
+tbls5096=timingByLsHists[136:138]
+lumi5096=lumiHists[136:138]
+thru5096=thruHists[87:89]
+
+runs5095= runs[132:136]
+tbls5095=timingByLsHists[132:136]
+lumi5095=lumiHists[132:136]
+thru5095=thruHists[83:87]
+
+runs5093= runs[129:132]
+tbls5093=timingByLsHists[129:132]
+lumi5093=lumiHists[129:132]
+thru5093=thruHists[80:83]
+
+runs5091= runs[126:129]
+tbls5091=timingByLsHists[126:129]
+lumi5091=lumiHists[126:129]
+thru5091=thruHists[77:80]
+
+runs5085= runs[121:126]
+tbls5085=timingByLsHists[121:126]
+lumi5085=lumiHists[121:126]
+thru5085=thruHists[72:77]
+
+runs5083= runs[117:121]
+tbls5083=timingByLsHists[117:121]
+lumi5083=lumiHists[117:121]
+thru5083=thruHists[68:72]
+
+runs5080= runs[114:117]
+tbls5080=timingByLsHists[114:117]
+lumi5080=lumiHists[114:117]
+thru5080=thruHists[65:68]
+
+runs5078= runs[111:114]
+tbls5078=timingByLsHists[111:114]
+lumi5078=lumiHists[111:114]
+thru5078=thruHists[62:65]
+
+runs5076= runs[105:111]
+tbls5076=timingByLsHists[105:111]
+lumi5076=lumiHists[105:111]
+thru5076=thruHists[56:62]
+
+runs5073= runs[99:105]
+tbls5073=timingByLsHists[99:105]
+lumi5073=lumiHists[99:105]
+thru5073=thruHists[50:56]
+
+runs5072= runs[95:99]
+tbls5072=timingByLsHists[95:99]
+lumi5072=lumiHists[95:99]
+thru5072=thruHists[46:50]
+
+runs5071= runs[93:95]
+tbls5071=timingByLsHists[93:95]
+lumi5071=lumiHists[93:95]
+thru5071=thruHists[44:46]
+
+runs5069= runs[90:93]
+tbls5069=timingByLsHists[90:93]
+lumi5069=lumiHists[90:93]
+thru5069=thruHists[41:44]
+
+runs5060= runs[88:90]
+tbls5060=timingByLsHists[88:90]
+lumi5060=lumiHists[88:90]
+thru5060=thruHists[39:41]
+
+runs5056= runs[87:88]
+tbls5056=timingByLsHists[87:88]
+lumi5056=lumiHists[87:88]
+thru5056=thruHists[38:39]
+
+runs5052= runs[80:87]
+tbls5052=timingByLsHists[80:87]
+lumi5052=lumiHists[80:87]
+thru5052=thruHists[31:38]
+
+runs5048= runs[77:80]
+tbls5048=timingByLsHists[77:80]
+lumi5048=lumiHists[77:80]
+thru5048=thruHists[28:31]
+
+runs5045= runs[70:77]
+tbls5045=timingByLsHists[70:77]
+lumi5045=lumiHists[70:77]
+thru5045=thruHists[21:28]
+
+runs5043= runs[61:70]
+tbls5043=timingByLsHists[61:70]
+lumi5043=lumiHists[61:70]
+thru5043=thruHists[12:21]
+
+runs5038= runs[59:61]
+tbls5038=timingByLsHists[59:61]
+lumi5038=lumiHists[59:61]
+thru5038=thruHists[10:12]
+
+runs5030= runs[57:59]
+tbls5030=timingByLsHists[57:59]
+lumi5030=lumiHists[57:59]
+thru5030=thruHists[8:10]
 
 runs5029= runs[55:57]
 tbls5029=timingByLsHists[55:57]
@@ -247,15 +357,30 @@ def plotTimingVLumi(tHist,lHist,run):
         g.SetPoint(i,lumis[i],times[i])
 
 #    g.SetMarkerStyle(22)
+    #fiitng
+    lin = TF1("lin","pol1",3000,12000)
+    line = TLine(3000,205,12000,205)
+    line.SetLineColor(kRed)
+    quad = TF1("quad","pol2",3000,12000)
+    g.Fit(quad)
+    quad.SetLineColor(kGreen)
+    g.Fit(lin)
+    lin.SetLineColor(kBlue)
+    #dummy point pst fit for drawing
+    g.SetPoint(npoints,50000,500)
     g.SetTitle("Processing Time vs. Luminosity")
     g.GetXaxis().SetTitle("Instantaneous Luminosity (e30 cm^{-2})")
+    g.GetXaxis().SetRangeUser(3000,12000)
     g.GetYaxis().SetTitle("Processing time (ms)")
+    g.GetYaxis().SetRangeUser(0,300)
     g.Draw("ap")
+    quad.Draw("same")
+    lin.Draw("same")
+    line.Draw("same")
     pdfname = "Timing_v_Lumi_Run%s.pdf" %run
     c1.Print(pdfname)
 
 def plotTimingVLumiAll(tHists,lHists,runs):
-
     c1 = TCanvas()
     if len(runs)>5:
         leg = TLegend(0.1,0.3,0.5,0.9)
@@ -265,11 +390,12 @@ def plotTimingVLumiAll(tHists,lHists,runs):
     leg.SetBorderSize(0)
     it=0
     graphs =[]
-    for run in runs:
+    for run in runs:  
+        
         g = getGraph(tHists[it],lHists[it])
         #add a dummy point to make plotting axis range possible
         g.SetPoint(g.GetN(),10000,2000)
-        g.GetXaxis().SetRangeUser(0,10000)
+        g.GetXaxis().SetRangeUser(0,16000)
         g.GetYaxis().SetRangeUser(0,300)
         g.SetMarkerColor(it+1)
         g.SetFillColor(it+1)
@@ -292,16 +418,57 @@ def plotTimingVLumiAll(tHists,lHists,runs):
     leg.Draw("same")
     c1.Print("Timing_vLumi_Runs%s-%s.pdf" % (runs[0],runs[len(runs)-1]))
 
+def plotTimingVPileupAll(tHists,lHists,runs):
+    c1 = TCanvas()
+    if len(runs)>5:
+        leg = TLegend(0.1,0.3,0.5,0.9)
+    else:
+        leg = TLegend(0.1,0.6,0.5,0.9)
+    leg.SetFillStyle(0)
+    leg.SetBorderSize(0)
+    it=0
+    graphs =[]
+    for run in runs:  
+        
+        g = getPileupGraph(tHists[it],lHists[it])
+        #add a dummy point to make plotting axis range possible
+        g.SetPoint(g.GetN(),10000,2000)
+        g.GetXaxis().SetRangeUser(0,60)
+        g.GetYaxis().SetRangeUser(0,300)
+        g.SetMarkerColor(it+1)
+        g.SetFillColor(it+1)
+        graphs.append(g)
+        it+=1
+
+    it=0
+    for graph in graphs:
+        graph.SetTitle("Processing Time vs. Pileup")
+        graph.GetXaxis().SetTitle("Pileup")
+        graph.GetYaxis().SetTitle("Processing time (ms)")
+        graph.GetXaxis().SetRangeUser(0,60)
+        leg.AddEntry(graph,"Run: %s" % runs[it],"f")
+        if it ==0:
+            graph.Draw("ap")
+        else:
+            graph.Draw("psame")
+        it+=1
+    
+    leg.Draw("same")
+    c1.Print("Timing_vPileup_Runs%s-%s.pdf" % (runs[0],runs[len(runs)-1]))
+
+
 def getGraph(tHist,lHist):
     times = []
     lumis = []
     npoints=0
     for i in range(0,tHist.GetNbinsX()):
+        #print "timing: %f ; lumi: %f" % (tHist.GetBinContent(i+1),lHist.GetBinContent(i+1))
         if (tHist.GetBinContent(i+1)>0 or i<20):
-           
+            
             times.append(tHist.GetBinContent(i+1))
             lumis.append(lHist.GetBinContent(i+1))
             npoints+=1
+            
     g = TGraph(npoints)
     for i in range(0,npoints):
         g.SetPoint(i,lumis[i],times[i])
@@ -309,6 +476,32 @@ def getGraph(tHist,lHist):
 #    g.SetMarkerStyle(22)
     g.SetTitle("Processing Time vs. Luminosity")
     g.GetXaxis().SetTitle("Instantaneous Luminosity (e30 cm^{-2})")
+    g.GetYaxis().SetTitle("Processing time (ms)")
+    return g
+
+
+
+def getPileupGraph(tHist,lHist):
+    times = []
+    lumis = []
+    npoints=0
+    for i in range(0,tHist.GetNbinsX()):
+        #print "timing: %f ; lumi: %f" % (tHist.GetBinContent(i+1),lHist.GetBinContent(i+1))
+        if (tHist.GetBinContent(i+1)>0 or i<20):
+            
+            times.append(tHist.GetBinContent(i+1))
+            pileup = (lHist.GetBinContent(i+1) * 71.3*.54)/ 11246.0
+            print pileup
+            lumis.append(pileup)
+            npoints+=1
+            
+    g = TGraph(npoints)
+    for i in range(0,npoints):
+        g.SetPoint(i,lumis[i],times[i])
+
+#    g.SetMarkerStyle(22)
+    g.SetTitle("Processing Time vs. Pileup")
+    g.GetXaxis().SetTitle("Instantaneous Pileup")
     g.GetYaxis().SetTitle("Processing time (ms)")
     return g
 
@@ -431,15 +624,64 @@ plotLogTiming(timingHists)
 plotOverFlow(timingHists)
 plotTimingVRun(timingHists)
 plotTimingVOverFlow(timingHists)
+
 it=0
 for run in runs:
     plotTimingVLumi(timingByLsHists[it],lumiHists[it],run)
     it+=1
 
 plotTimingVLumiAll(timingByLsHists,lumiHists,runs)
+
+plotThroughputVLumi(thru5101,lumi5101,runs5101)
+plotThroughputVLumi(thru5097,lumi5097,runs5097)
+plotThroughputVLumi(thru5096,lumi5096,runs5096)
+plotThroughputVLumi(thru5095,lumi5095,runs5095)
+plotThroughputVLumi(thru5093,lumi5093,runs5093)
+plotThroughputVLumi(thru5091,lumi5091,runs5091)
+plotThroughputVLumi(thru5085,lumi5085,runs5085)
+plotThroughputVLumi(thru5083,lumi5083,runs5083)
+plotThroughputVLumi(thru5080,lumi5080,runs5080)
+plotThroughputVLumi(thru5078,lumi5078,runs5078)
+plotThroughputVLumi(thru5076,lumi5076,runs5076)
+plotThroughputVLumi(thru5073,lumi5073,runs5073)
+plotThroughputVLumi(thru5072,lumi5072,runs5072)
+plotThroughputVLumi(thru5071,lumi5071,runs5071)
+plotThroughputVLumi(thru5069,lumi5069,runs5069)
+plotThroughputVLumi(thru5060,lumi5060,runs5060)
+plotThroughputVLumi(thru5056,lumi5056,runs5056)
+plotThroughputVLumi(thru5052,lumi5052,runs5052)
+plotThroughputVLumi(thru5048,lumi5048,runs5048)
+plotThroughputVLumi(thru5045,lumi5045,runs5045)
+plotThroughputVLumi(thru5043,lumi5043,runs5043)
+plotThroughputVLumi(thru5038,lumi5038,runs5038)
+plotThroughputVLumi(thru5030,lumi5030,runs5030)
 plotThroughputVLumi(thru5029,lumi5029,runs5029)
 plotThroughputVLumi(thru5028,lumi5028,runs5028)
 plotThroughputVLumi(thru5027,lumi5027,runs5027)
+
+plotTimingVLumiAll(tbls5101,lumi5101,runs5101)
+plotTimingVLumiAll(tbls5097,lumi5097,runs5097)
+plotTimingVLumiAll(tbls5096,lumi5096,runs5096)
+plotTimingVLumiAll(tbls5095,lumi5095,runs5095)
+plotTimingVLumiAll(tbls5093,lumi5093,runs5093)
+plotTimingVLumiAll(tbls5091,lumi5091,runs5091)
+plotTimingVLumiAll(tbls5085,lumi5085,runs5085)
+plotTimingVLumiAll(tbls5083,lumi5083,runs5083)
+plotTimingVLumiAll(tbls5080,lumi5080,runs5080)
+plotTimingVLumiAll(tbls5078,lumi5078,runs5078)
+plotTimingVLumiAll(tbls5076,lumi5076,runs5076)
+plotTimingVLumiAll(tbls5073,lumi5073,runs5073)
+plotTimingVLumiAll(tbls5072,lumi5072,runs5072)
+plotTimingVLumiAll(tbls5071,lumi5071,runs5071)
+plotTimingVLumiAll(tbls5069,lumi5069,runs5069)
+plotTimingVLumiAll(tbls5060,lumi5060,runs5060)
+plotTimingVLumiAll(tbls5056,lumi5056,runs5056)
+plotTimingVLumiAll(tbls5052,lumi5052,runs5052)
+plotTimingVLumiAll(tbls5048,lumi5048,runs5048)
+plotTimingVLumiAll(tbls5045,lumi5045,runs5045)
+plotTimingVLumiAll(tbls5043,lumi5043,runs5043)
+plotTimingVLumiAll(tbls5038,lumi5038,runs5038)
+plotTimingVLumiAll(tbls5030,lumi5030,runs5030)
 plotTimingVLumiAll(tbls5029,lumi5029,runs5029)
 plotTimingVLumiAll(tbls5028,lumi5028,runs5028)
 plotTimingVLumiAll(tbls5027,lumi5027,runs5027)
@@ -450,6 +692,101 @@ plotTimingVLumiAll(tbls5020,lumi5020,runs5020)
 plotTimingVLumiAll(tbls5017,lumi5017,runs5017)
 plotTimingVLumiAll(tbls5013,lumi5013,runs5013)
 plotTimingVLumiAll(tbls5005,lumi5005,runs5005)
+
+plotTimingVPileupAll(tbls5101,lumi5101,runs5101)
+plotTimingVPileupAll(tbls5097,lumi5097,runs5097)
+plotTimingVPileupAll(tbls5096,lumi5096,runs5096)
+plotTimingVPileupAll(tbls5095,lumi5095,runs5095)
+plotTimingVPileupAll(tbls5093,lumi5093,runs5093)
+plotTimingVPileupAll(tbls5091,lumi5091,runs5091)
+plotTimingVPileupAll(tbls5085,lumi5085,runs5085)
+plotTimingVPileupAll(tbls5083,lumi5083,runs5083)
+plotTimingVPileupAll(tbls5080,lumi5080,runs5080)
+plotTimingVPileupAll(tbls5078,lumi5078,runs5078)
+plotTimingVPileupAll(tbls5076,lumi5076,runs5076)
+plotTimingVPileupAll(tbls5073,lumi5073,runs5073)
+plotTimingVPileupAll(tbls5072,lumi5072,runs5072)
+plotTimingVPileupAll(tbls5071,lumi5071,runs5071)
+plotTimingVPileupAll(tbls5069,lumi5069,runs5069)
+plotTimingVPileupAll(tbls5060,lumi5060,runs5060)
+plotTimingVPileupAll(tbls5056,lumi5056,runs5056)
+plotTimingVPileupAll(tbls5052,lumi5052,runs5052)
+plotTimingVPileupAll(tbls5048,lumi5048,runs5048)
+plotTimingVPileupAll(tbls5045,lumi5045,runs5045)
+plotTimingVPileupAll(tbls5043,lumi5043,runs5043)
+plotTimingVPileupAll(tbls5038,lumi5038,runs5038)
+plotTimingVPileupAll(tbls5030,lumi5030,runs5030)
+plotTimingVPileupAll(tbls5029,lumi5029,runs5029)
+plotTimingVPileupAll(tbls5028,lumi5028,runs5028)
+plotTimingVPileupAll(tbls5027,lumi5027,runs5027)
+plotTimingVPileupAll(tbls5026,lumi5026,runs5026)
+plotTimingVPileupAll(tbls5024,lumi5024,runs5024)
+plotTimingVPileupAll(tbls5021,lumi5021,runs5021)
+plotTimingVPileupAll(tbls5020,lumi5020,runs5020)
+plotTimingVPileupAll(tbls5017,lumi5017,runs5017)
+plotTimingVPileupAll(tbls5013,lumi5013,runs5013)
+plotTimingVPileupAll(tbls5005,lumi5005,runs5005)
+
+
+it=0
+for run in runs5071:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5071[it],tbls5071[it],run)
+    it+=1
+
+it=0
+for run in runs5069:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5069[it],tbls5069[it],run)
+    it+=1
+
+it=0
+for run in runs5060:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5060[it],tbls5060[it],run)
+    it+=1
+
+it=0
+for run in runs5056:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5056[it],tbls5056[it],run)
+    it+=1
+
+it=0
+for run in runs5052:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5052[it],tbls5052[it],run)
+    it+=1
+
+it=0
+for run in runs5048:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5048[it],tbls5048[it],run)
+    it+=1
+
+it=0
+for run in runs5045:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5045[it],tbls5045[it],run)
+    it+=1
+
+it=0
+for run in runs5043:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5043[it],tbls5043[it],run)
+    it+=1
+
+it=0
+for run in runs5038:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5038[it],tbls5038[it],run)
+    it+=1
+
+it=0
+for run in runs5030:
+    print "it: %i run %s" % (it,run)
+    plotThroughputANDTiming(thru5030[it],tbls5030[it],run)
+    it+=1
 
 it=0
 for run in runs5029:
