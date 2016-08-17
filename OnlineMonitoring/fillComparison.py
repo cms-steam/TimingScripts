e91ba225cad42d381402314875e83c84349dea61
Clint <clint.a.richardson@gmail.com>
2016-08-17 12:46:14 +0200

    stable working version

--- OnlineMonitoring/fillComparison.py
+++ OnlineMonitoring/fillComparison.py
@@ -2,7 +2,7 @@
 
 import os
 import argparse
-parser = argparse.ArgumentParser(description='script to plot the timing vs. luminosity of different fills. Pass different fills as csv list and corresponding runs as csv list with a colon separating fills. The script relies on the dqm files existing and having been gotten via getDQM.py. Optionally pass names of hlt menus used for each fill. Usage: python fillComparison.py --fills fill1,fill2,etc --runs fill1run1,fill1run2:fill2run1,fill2run2,etc --hlt fill1hlt,fill2hlt.')
+parser = argparse.ArgumentParser(description='script to plot the timing vs. luminosity of different fills. Pass different fills as csv list and corresponding runs as csv list with a colon separating fills. The script relies on the dqm files existing and having been gotten via getDQM.py. Optionally pass names of hlt menus used for each fill. Usage: python fillComparison.py --fills fill1,fill2,etc --runs fill1run1,fill1run2:fill2run1,fill2run2,etc --hlt fill1hlt,fill2hlt')
 
 parser.add_argument("--fills",type=str,help='list of fill numbers (e.g 5010,5011,etc)',required=True,nargs=1)
 parser.add_argument("--runs",type=str,help='list of runs comma separated intrafill and colon separated interfill (e.g. 275000,275001:275100,275101)',required=True,nargs=1)
@@ -14,27 +14,28 @@ args = parser.parse_args()
 #from plotDQM import getGraph
 
 from ROOT import *
-def getGraph(tHist,lHist):
+
+def getGraph(tHists,lHists):
     times = []
     lumis = []
     npoints=0
-    for i in range(0,tHist.GetNbinsX()):
+    for tHist,lHist in zip(tHists,lHists):
+        for i in range(0,tHist.GetNbinsX()):
         #print "timing: %f ; lumi: %f" % (tHist.GetBinContent(i+1),lHist.GetBinContent(i+1))
-        if (tHist.GetBinContent(i+1)>0 or i<20):
+            if (tHist.GetBinContent(i+1)>0 or i<20):            
+                times.append(tHist.GetBinContent(i+1))
+                lumis.append(lHist.GetBinContent(i+1))
+                npoints+=1
             
-            times.append(tHist.GetBinContent(i+1))
-            lumis.append(lHist.GetBinContent(i+1))
-            npoints+=1
-            
-    g = TGraph(npoints)
-    for i in range(0,npoints):
-        g.SetPoint(i,lumis[i],times[i])
+        g = TGraph(npoints)
+        for i in range(0,npoints):
+            g.SetPoint(i,lumis[i],times[i])
 
 #    g.SetMarkerStyle(22)
-    g.SetTitle("Processing Time vs. Luminosity")
-    g.GetXaxis().SetTitle("Instantaneous Luminosity (e30 cm^{-2})")
-    g.GetYaxis().SetTitle("Processing time (ms)")
-    return g
+        g.SetTitle("Processing Time vs. Luminosity")
+        g.GetXaxis().SetTitle("Instantaneous Luminosity (e30 cm^{-2})")
+        g.GetYaxis().SetTitle("Processing time (ms)")
+        return g
 
 
 class fill(object):
@@ -44,24 +45,29 @@ class fill(object):
         self.menu = menu
         self.tHists = []
         self.lHists = []
-        self.graph = TMultiGraph()
+        self.lFiles = []
+        self.tFiles = []
+        self.graph = TGraph()
 
     def addTimeHist(self,f):
-        self.tHists.append( f.Get("/HLT/TimerService/Running 4 processes/event_byls"))
+        hist = f.Get("/HLT/TimerService/Running 4 processes/event_byls")
+        print hist
+        self.tHists.append(hist)
 
     def addLumiHist(self,f):
-        self.lHists.append( f.Get("/Scal/LumiScalers/Instant_Lumi"))
+        hist = f.Get("/Scal/LumiScalers/Instant_Lumi")
+        print hist
+        self.lHists.append(hist)
         
-    def SetMultiGraph(self):
-        for lh,th in zip(self.lHists,self.tHists):
-            self.graph.Add( getGraph(lh,th) )
+    def SetGraph(self):
+        self.graph = getGraph(self.tHists,self.lHists)
         self.graph.SetTitle("Processing Time vs. Luminosity; Luminosity (e30 cm^{-2}); Average Processing Time")
 
 #get fills
 fillnumbers = args.fills[0].split(',')
 runnumbers = args.runs[0].split(':')
 
-print fillnumbers, runnumbers
+
 
 #create list of fills
 fills = []
@@ -76,39 +82,57 @@ for f,it in zip(fillnumbers,range(0,1000)):
 
 
 #now add histograms to fills
+
 for f in fills:
+    # make file lists
     for r in f.runs:
         timefname = "timeByLs_rootFiles/Global__Online__ALL__run%s.root" %r
-        f.addTimeHist(TFile(timefname))
+        f.tFiles.append(TFile(timefname))
         lumifname = "lumi_rootFiles/Global__Online__ALL__run%s.root" %r
-        f.addLumiHist(TFile(lumifname))
+        f.lFiles.append(TFile(lumifname))
+#        lhist = flumi.Get("/Scal/LumiScalers/Instant_Lumi")
+        #f.lHists.append(lhist)
+
+    #make histograms
+    for lf in f.lFiles:
+        f.lHists.append( lf.Get("/Scal/LumiScalers/Instant_Lumi"))
+    for tf in f.tFiles:
+        f.tHists.append( tf.Get("/HLT/TimerService/Running 4 processes/event_byls") )
+    print f.lHists, f.tHists
 
 #set multigraph
 for f in fills:
-    f.SetMultiGraph()
+    f.SetGraph()
     
 #now draw
 
 c = TCanvas()
-leg = TLegend()
+leg = TLegend(0.1,0.5,0.4,0.9)
 
 for f,it in zip(fills, range(0,10000)):
     f.graph.SetMarkerColor(it+1)
+    f.graph.SetFillColor(it+1)
     if it==0:
-        f.graph.Draw("APL")
+        f.graph.Draw("AP")
+        #f.graph.GetXaxis().SetRangeUser(0,15000)
+        #f.graph.GetYaxis().SetRangeUser(0,250)
+        #f.graph.Draw("AP")
     else:
         f.graph.Draw("p same")
     legtext ="Fill %s" % f.number
-    if f.hlt is not '':
-        legtext+="; HLT Menu: %s" % f.hlt
-    leg.AddEntry(f.graph,legtext,"p")
+    if f.menu is not '':
+        legtext+="; HLT Menu: %s" % f.menu
+    leg.AddEntry(f.graph,legtext,"f")
 
+leg.SetBorderSize(0)
+leg.SetFillStyle(0)
+leg.Draw("same")
 
-pdfname = "FillComparison_"
+pdfname = "FillComparison"
 
 for f in fills:
-    pdfname+="%s_" % fill.number
+    pdfname+="_%s" % f.number
 
 pdfname+=".pdf"
 
-c.Print()
+c.Print(pdfname)
