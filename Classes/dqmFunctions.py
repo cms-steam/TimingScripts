33ca6e8305cc8dd0712ffb024e43444ca2b2aee9
Clint <clint.a.richardson@gmail.com>
2016-08-17 15:27:47 +0200

    first commit

---
+++ Classes/dqmFunctions.py
@@ -0,0 +1,13 @@
+
+
+def inGolden(jsonfile,run,lumi):
+    #boolean to return
+    golden=False
+
+    if u'%i' % run in jsonfile:
+        lumis=jsonfile[u'%i' % run]
+        for lumirange in lumis:
+            if lumi > lumirange[0] and lumi < lumirange[1]:
+                golden=True
+
+    return golden
