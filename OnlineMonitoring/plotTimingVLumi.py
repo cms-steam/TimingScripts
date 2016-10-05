82b8a8106738febe2f1a4d67bc9de7aeea217887
Clint <clint.a.richardson@gmail.com>
2016-10-05 23:24:28 +0200

    fixed typos

--- OnlineMonitoring/plotTimingVLumi.py
+++ OnlineMonitoring/plotTimingVLumi.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+from ROOT import *
+
 parser = argparse.ArgumentParser(description='script to plot the timing for luminosity ranges specified by the user based on provided root files - if the needed root files do not exist run getDQM.py first for the relevant runs. Specify the luminosity bins as comma separated list. Usage: python plotTimingVLumi.py --runs run1,run2,run3 --lumibins ')
 
 parser.add_argument("--runs",type=str,help='list of runs comma separated',required=True,nargs=1)
@@ -8,16 +10,22 @@ parser.add_argument("--lumibins",type=str,help='comma separated list of lumi bin
 args = parser.parse_args()
 
 def getAverageTimeForLumiRange(lumilow,lumihigh,lhists,thists):
+    print lhists
     ntimes=0
     time=0.0
-    for lhist,thist in zip(lhists,thists):
+    for thist,lhist in zip(thists,lhists):
         for i in range(1,lhist.GetNbinsX()+1):
-            lumi = lhist.GetBinContent(i):
+            lumi = lhist.GetBinContent(i)
+            print lumi
             if lumi>=lumilow and lumi<lumihigh:
                 time+=thist.GetBinContent(i)
                 ntimes+=1
 
-    time = time/float(ntimes)
+    if ntimes!=0:
+        time = time/float(ntimes)
+    else:
+        time=0.0
+
     return time
 
 #get runs
@@ -30,22 +38,32 @@ lumibins = args.lumibins[0].split(',')
 #get timing and lumi hists
 thists = []
 lhists = []
+tfiles = []
+lfiles = []
 for run in runs:
-    timef = TFile("timeByLs_rootFiles/Global__Online__ALL__run%s.root" %r)
+    timef = TFile("timeByLs_rootFiles/Global__Online__ALL__run%s.root" %run)
+    tfiles.append(timef)
+    lumif = TFile("lumi_rootFiles/Global__Online__ALL__run%s.root" %run)
+    lfiles.append(lumif)
+
+for tfile in tfiles:
     timehist = timef.Get("/HLT/TimerService/Running 4 processes/event_byls")
     thists.append(timehist)
-    lumif = TFile("lumi_rootFiles/Global__Online__ALL__run%s.root" %r)
+
+for lfile in lfiles:
     lumihist = lumif.Get("/Scal/LumiScalers/Instant_Lumi")
     lhists.append(lumihist)
 
+print thists
+print lhists
 
 #now get average timing for each lumibin
 times=[] #will hold average time for each bin
 lumis=[] #note that this will be the central value of each bin
 for i in range(0,len(lumibins)-1): # len(lumis)-1 so we don't access outside the list
-    time =  getAverageTimeForLumiRange(lumibins[i],lumibins[i+1],thists,lhists)
+    time =  getAverageTimeForLumiRange(lumibins[i],lumibins[i+1],lhists,thists)
     times.append(time)
-    lumi = (lumibins[i]+lumibins[i+1])/2.0
+    lumi = (int(lumibins[i])+int(lumibins[i+1]))/2.0
     lumis.append(lumi)
 
 
